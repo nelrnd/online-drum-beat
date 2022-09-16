f23fb6d8aa20d3d306a15601478ac6fb33599bee
Nel <nel.renaudin@gmail.com>
2022-09-16 16:30:16 +0200

    Add sounds and buttons to play sounds

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('working fine');
+const buttons = {
+  kick: document.getElementById('kick'),
+  snare: document.getElementById('snare'),
+  clap: document.getElementById('clap'),
+  hihat: document.getElementById('hihat'),
+  openhat: document.getElementById('openhat')
+};
+
+const sounds = {
+  kick: document.getElementById('kickSound'),
+  snare: document.getElementById('snareSound'),
+  clap: document.getElementById('clapSound'),
+  hihat: document.getElementById('hihatSound'),
+  openhat: document.getElementById('openhatSound')
+};
+
+for (let button in buttons) {
+  buttons[button].addEventListener('click', event => {
+    sounds[event.target.id].currentTime = 0;
+    sounds[event.target.id].play();
+  })
+}
