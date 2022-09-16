cb4df7dd4f4eb4ca5c127c49bbf9ea0d4bed9921
Nel <nel.renaudin@gmail.com>
2022-09-16 18:05:05 +0200

    Implement bar table, handle clicks to add and remove sound from bar

--- script.js
+++ script.js
@@ -20,3 +20,59 @@ for (let button in buttons) {
     sounds[event.target.id].play();
   })
 }
+
+const playBtn = document.querySelector('button#play');
+
+playBtn.addEventListener('click', () => {
+  setInterval(playBar, 200);
+});
+
+const bars = document.querySelectorAll('td');
+
+bars.forEach(bar => {
+  bar.addEventListener('click', event => {
+    event.target.classList.toggle('full');
+  })
+})
+
+const cols = document.querySelectorAll('table col:first-child ~ col');
+
+let currentBar = 1;
+
+/*
+function play() {
+  if (currentBar > 0) {
+    cols[(currentBar - 1)].classList.remove('black');
+  } else if (currentBar === 0) {
+    cols[(cols.length - 1)].classList.remove('black');
+  }
+
+  cols[currentBar].classList.add('black');
+  sounds.kick.currentTime = 0;
+  sounds.kick.play();
+  currentBar++;
+
+  if (currentBar === cols.length) {
+    currentBar = 0;
+  }
+}
+*/
+
+
+function playBar() {
+  if (currentBar > 4) {
+    currentBar = 1;
+  }
+
+  const colBars = document.querySelectorAll(`td:nth-of-type(${currentBar})`);
+
+  for (let i = 0; i < colBars.length; i++) {
+    const corrSound = document.querySelector(`tr:nth-of-type(${i + 1}) th`).id;
+    if (colBars[i].className === 'full') {
+      sounds[corrSound].currentTime = 0;
+      sounds[corrSound].play();
+    }
+  }
+
+  currentBar++;
+}
